ff6842585b8ef624c6150605bd96e1d8d6ff8b29
dungle040699 <qdung.le99@gmail.com>
2024-08-30 12:14:01 +0700

    init bff server

---
+++ bff/src/test/java/server/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package server.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
