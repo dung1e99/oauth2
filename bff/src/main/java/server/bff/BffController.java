473cf7bc47bd3e0e263c1870429930446fbea62f
dungle040699 <qdung.le99@gmail.com>
2024-09-02 17:32:09 +0700

    ok

---
+++ bff/src/main/java/server/bff/BffController.java
@@ -0,0 +1,13 @@
+package server.bff;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BffController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
