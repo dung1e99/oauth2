d968e3d1abb48d1bc60299cd95fdab6e2f3c4753
dungle040699 <qdung.le99@gmail.com>
2024-09-06 13:32:31 +0700

    ok now

--- bff/src/main/java/server/bff/BffController.java
+++ bff/src/main/java/server/bff/BffController.java
@@ -3,7 +3,7 @@ package server.bff;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/")
 public class BffController {
 
     @GetMapping
