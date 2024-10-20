d6aab490a0cd68dd1db0e023ca2a43f5ce739e9a
chipu-le <qdung.le99@gmail.com>
2024-10-20 19:13:53 +0700

    refresh-token on-going

---
+++ bff/src/main/java/server/bff/WebClientConfig.java
@@ -0,0 +1,58 @@
+package server.bff;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public OAuth2AuthorizedClientProvider auth2AuthorizedClientProvider() {
+
+        return OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .refreshToken()
+                .clientCredentials()
+                .build();
+    }
+
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                          OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .refreshToken()
+                .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository,
+                authorizedClientRepository);
+
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+    @Bean
+    public ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client(OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        return new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+    }
+
+    @Bean
+    public WebClient webClientConfig(ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client) {
+
+        return WebClient.builder()
+                .filter(oauth2Client)
+                .build();
+    }
+}
